3db7f73840a2a84c43c5e42d8f3d0f9ebe71a65a
Manuel Junior <manuel.jr@outlook.com>
2025-03-16 22:34:46 +0000

    teste

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env.VITE_BASE_URL': JSON.stringify(process.env.VITE_BASE_URL)
+    'process.env.VITE_BASE_URL': JSON.stringify('https://to-do-list-back-end-one.vercel.app')
   }
 })
