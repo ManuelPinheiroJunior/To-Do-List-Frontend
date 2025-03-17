132047ef5f3dd8a86807e68b29145f96e6131867
Manuel Junior <manuel.jr@outlook.com>
2025-03-17 02:28:47 +0000

    test

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,5 +3,5 @@ export * from "./taskTypes";
 export * from "./userTypes";
 export * from "./TaskListTypes";
 export * from "./book";
-export * from "./CompleteTaskTypes";
+export * from "./completeTaskTypes";
 export * from "./apiResponseTypes";
