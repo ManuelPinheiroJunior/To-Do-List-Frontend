ecd769bcd97e36343300260c413e425eb7bd366e
Manuel Junior <manuel.jr@outlook.com>
2025-03-21 18:51:35 +0000

    Others adjust in buttons

--- src/components/CompleteTaskList.tsx
+++ src/components/CompleteTaskList.tsx
@@ -1,19 +1,22 @@
-
 import { Card, Row, Col, Button } from "react-bootstrap";
 import { CompleteTaskProps } from "../types/completeTaskTypes";
+import { useTranslation } from "../hooks/usetTranslation";
 
 
 const CompleteTaskList = (props: CompleteTaskProps) => {
+  const t = useTranslation();
+
   return (
     <Card className="shadow-lg border-0 rounded-3 p-4 mb-3">
       <Row className="align-items-center">
-    
         <Col xs={8} className="d-flex flex-column">
-          <h5 className="fw-bold text-dark">{props.task}</h5>
+          <h5 className="fw-bold text-dark d-flex align-items-center">
+            {props.task}
+             ✔
+          </h5>
           <small className="text-muted">{props.dateTime}</small>
         </Col>
 
-       
         <Col xs={4} className="d-flex justify-content-end">
           <Button 
             variant="outline-danger" 
@@ -21,7 +24,7 @@ const CompleteTaskList = (props: CompleteTaskProps) => {
             className="fw-bold"
             onClick={() => props.deleteTodo(props.id)}
           >
-            ✖ Delete
+            ✖ {t.Delete}
           </Button>
         </Col>
       </Row>
